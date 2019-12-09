78f6a61647cca3c5cf6ed651d7a5701604715006
ArtemGord1728 <artemseraf01@mail.ru>
2019-12-09 06:22:00 +0300

    added new method

--- enclave-testy/src/enclave_asserts.cpp
+++ enclave-testy/src/enclave_asserts.cpp
@@ -21,4 +21,39 @@ namespace enclave_testy
 		std::cout << string_stream.str() << std::endl;
 		system(color_txt);
 	}
+
+	template <typename FirstType, typename SecondType>
+	void assert_equals(const std::string message, const FirstType& first, const SecondType& second)
+	{
+		std::ostringstream string_stream;
+		const char* color_txt;
+		if (first != second)
+		{
+			string_stream << __FILE__ << " " << " " << "Line: " << __LINE__ << std::endl;
+			string_stream << message << first << " != " << second;
+			color_txt = "color 4";
+		}
+		else
+		{
+			string_stream << "AssertEqual - OK";
+			color_txt = "color 2";
+		}
+		std::cout << string_stream.str() << std::endl;
+		system(color_txt);
+	}
+
+
+	//TODO
+	/*template <typename FirstType, typename SecondType>
+	bool assert_false(const FirstType& first, const SecondType& second)
+	{
+		return false;
+	}
+
+
+	template <typename FirstType, typename SecondType>
+	bool assert_true(const FirstType& first, const SecondType& second)
+	{
+		return true;
+	}*/
 }
