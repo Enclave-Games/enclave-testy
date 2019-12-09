74ce45efd727b3a5258ca782adc1ed885113395b
ArtemGord1728 <artemseraf01@mail.ru>
2019-12-09 17:26:30 +0300

    defined methods

--- enclave-testy/src/enclave_asserts.h
+++ enclave-testy/src/enclave_asserts.h
@@ -9,8 +9,29 @@
 
 namespace enclave_testy
 {
-	template <typename FirstType, typename SecondType>
-	void assert_equals(const FirstType& first, const SecondType& second);
+	class TestWrapper
+	{
+	public:
+		template <typename FirstType, typename SecondType>
+		void assert_equals(const FirstType& first, const SecondType& second);
+
+		template <typename FirstType, typename SecondType>
+		void assert_equals(const std::string message, const FirstType& first, const SecondType& second);
+
+		template <typename TestFunction>
+		void isolate_function_for_test(const TestFunction& _function);
+
+		template <typename TestFunction>
+		void isolate_function_for_test(const TestFunction& _function, const std::string& test_name);
+
+		template <typename FirstType, typename SecondType>
+		void assert_true(const FirstType& first, const SecondType& second);
+
+		virtual ~TestWrapper();
+	private:
+		int fail_count = 0;
+	};
+	
 }
 
 #endif
